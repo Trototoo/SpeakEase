131d01737a3472b9bc920c89a7b957f73e544d7d
Oleksandr Nazarenko <sa44ko@gmail.com>
2023-06-05 21:33:27 +0300

    Implemented initialize function.

--- app/src/main/java/com/example/speakease/ChatActivity.kt
+++ app/src/main/java/com/example/speakease/ChatActivity.kt
@@ -1,11 +1,57 @@
 package com.example.speakease
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.speakease.adapter.MessagesAdapter
+import com.example.speakease.databinding.ActivityChatBinding
+import com.example.speakease.model.Message
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 
 class ChatActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChatBinding
+    private lateinit var adapter: MessagesAdapter
+    private lateinit var messages: ArrayList<Message>
+    private lateinit var senderRoom: String
+    private lateinit var receiverRoom: String
+    private lateinit var database: FirebaseDatabase
+    private lateinit var storage: FirebaseStorage
+    private lateinit var dialog: ProgressDialog
+    private lateinit var senderUid: String
+    private lateinit var receiverUid: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+    }
+
+    private fun initialize() {
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        database = FirebaseDatabase.getInstance()
+        storage = FirebaseStorage.getInstance()
+        dialog = ProgressDialog(this).apply {
+            setMessage("Uploading Image...")
+            setCancelable(false)
+        }
+        messages = ArrayList()
+
+        receiverUid = intent.getStringExtra("uid")!!
+        senderUid = FirebaseAuth.getInstance().uid!!
+
+        senderRoom = senderUid + receiverUid
+        receiverRoom = receiverUid + senderUid
+
+        adapter = MessagesAdapter(this, messages, senderRoom, receiverRoom)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 }
