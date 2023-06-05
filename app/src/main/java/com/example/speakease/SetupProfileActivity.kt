c08704e04360d2bed82b2c98008de146c3abf5a3
Oleksandr Nazarenko <sa44ko@gmail.com>
2023-06-05 20:08:47 +0300

    Implemented initFirebase function.

--- app/src/main/java/com/example/speakease/SetupProfileActivity.kt
+++ app/src/main/java/com/example/speakease/SetupProfileActivity.kt
@@ -2,10 +2,24 @@ package com.example.speakease
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 
 class SetupProfileActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+    private lateinit var storage: FirebaseStorage
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setup_profile)
+    }
+
+    private fun initFirebase() {
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        storage = FirebaseStorage.getInstance()
     }
 }
