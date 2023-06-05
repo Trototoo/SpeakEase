257b5ac29d0624ea30e0982071083d1c02973082
Oleksandr Nazarenko <sa44ko@gmail.com>
2023-06-05 20:41:40 +0300

    Implemented initialize function.

--- app/src/main/java/com/example/speakease/MainActivity.kt
+++ app/src/main/java/com/example/speakease/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.speakease
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.speakease.databinding.ActivityMainBinding
+import com.example.speakease.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var database: FirebaseDatabase
+    private lateinit var users: ArrayList<User>
+    private lateinit var dialog: ProgressDialog
+    private lateinit var user: User
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun initialize() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        dialog = ProgressDialog(this).apply {
+            setMessage("Uploading Image...")
+            setCancelable(false)
+        }
+
+        database = FirebaseDatabase.getInstance()
+        users = ArrayList()
+
+        val currentUserUid = FirebaseAuth.getInstance().uid!!
+
+        database.reference.child("users")
+            .child(currentUserUid)
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    user = snapshot.getValue(User::class.java)!!
+                }
+
+                override fun onCancelled(error: DatabaseError) {}
+            })
     }
 }
