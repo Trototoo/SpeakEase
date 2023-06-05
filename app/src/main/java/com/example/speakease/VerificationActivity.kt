076c069400c93d2cfe441bb2a37089497b1b500f
Oleksandr Nazarenko <sa44ko@gmail.com>
2023-06-05 18:44:55 +0300

    Implemented VerificationActivity view.
    VerificationActivity takes our number and opens OTPActivity.

--- app/src/main/java/com/example/speakease/VerificationActivity.kt
+++ app/src/main/java/com/example/speakease/VerificationActivity.kt
@@ -1,11 +1,37 @@
 package com.example.speakease
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.speakease.databinding.ActivityVerificationBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class VerificationActivity : AppCompatActivity() {
+
+    var binding : ActivityVerificationBinding? = null
+
+    var auth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_verification)
+        binding = ActivityVerificationBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        if (auth!!.currentUser != null) {
+            val intent = Intent(this@VerificationActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        supportActionBar?.hide()
+
+        binding!!.editNumber.requestFocus()
+        binding!!.continueBtn.setOnClickListener {
+            val intent = Intent(this@VerificationActivity, OTPActivity::class.java)
+            intent.putExtra("phoneNumber", binding!!.editNumber.text.toString())
+            startActivity(intent)
+        }
     }
 }
