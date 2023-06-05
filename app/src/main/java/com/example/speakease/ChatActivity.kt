e9d9d1f736992c7b28ec9acab0610409050d5814
Oleksandr Nazarenko <sa44ko@gmail.com>
2023-06-05 21:38:25 +0300

    Updated onCreate function.

--- app/src/main/java/com/example/speakease/ChatActivity.kt
+++ app/src/main/java/com/example/speakease/ChatActivity.kt
@@ -39,7 +39,10 @@ class ChatActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+
+        initialize()
+        setupViewElements()
+        fetchAndSetChatMessages()
     }
 
     private fun initialize() {
