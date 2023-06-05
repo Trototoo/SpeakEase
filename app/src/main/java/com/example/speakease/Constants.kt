9ace073e2f8f3f12c72c975d4a677d000a1bc714
Oleksandr Nazarenko <sa44ko@gmail.com>
2023-06-05 21:06:27 +0300

    Updated Constants.kt.
    Added USER_PRESENCE = "presence".

--- app/src/main/java/com/example/speakease/Constants.kt
+++ app/src/main/java/com/example/speakease/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
     const val IMAGE_PICK_REQUEST_CODE = 45
     const val PROFILE_PATH = "Profile"
     const val USERS_PATH = "users"
+    const val USER_PRESENCE = "presence"
 }
