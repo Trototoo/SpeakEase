fc61fe442a3d154880847c4d9282b3a5885210ca
Oleksandr Nazarenko <sa44ko@gmail.com>
2023-06-05 19:38:27 +0300

    Updated OTPActivity.kt.
    Added null safety.
    Decomposed my code into smaller different functions.
    Used also block to create Intent.

--- app/src/main/java/com/example/speakease/OTPActivity.kt
+++ app/src/main/java/com/example/speakease/OTPActivity.kt
@@ -21,35 +21,52 @@ import java.util.concurrent.TimeUnit
 
 class OTPActivity : AppCompatActivity() {
 
-    var binding: ActivityOtpactivityBinding? = null
-    var verificationId: String? = null
-    var auth: FirebaseAuth? = null
-    var dialog: ProgressDialog? = null
+    private lateinit var binding: ActivityOtpactivityBinding
+    private lateinit var verificationId: String
+    private lateinit var auth: FirebaseAuth
+    private lateinit var dialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOtpactivityBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
-        dialog = ProgressDialog(this@OTPActivity)
-        dialog!!.setMessage("Sending OTP...")
-        dialog!!.setCancelable(false)
-        dialog!!.show()
-        auth = FirebaseAuth.getInstance()
-        supportActionBar?.hide()
-        val phoneNumber = intent.getStringExtra("phoneNumber")
-        binding!!.phoneLble.text = "Verify $phoneNumber"
+        setContentView(binding.root)
 
-        val options = PhoneAuthOptions.newBuilder(auth!!)
-            .setPhoneNumber(phoneNumber!!)
+        setupProgressDialog()
+        hideActionBar()
+
+        auth = FirebaseAuth.getInstance()
+
+        val phoneNumber = intent.getStringExtra("phoneNumber") ?: ""
+        binding.phoneLble.text = "Verify $phoneNumber"
+
+        verifyPhoneNumber(phoneNumber)
+        handleOtpCompletion()
+    }
+
+    private fun setupProgressDialog() {
+        dialog = ProgressDialog(this@OTPActivity).apply {
+            setMessage("Sending OTP...")
+            setCancelable(false)
+            show()
+        }
+    }
+
+    private fun hideActionBar() {
+        supportActionBar?.hide()
+    }
+
+    private fun verifyPhoneNumber(phoneNumber: String) {
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber)
             .setTimeout(60L, TimeUnit.SECONDS)
             .setActivity(this@OTPActivity)
             .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
                 override fun onVerificationCompleted(p0: PhoneAuthCredential) {
-                    TODO("Not yet implemented")
+                    // TODO("Not yet implemented")
                 }
 
                 override fun onVerificationFailed(p0: FirebaseException) {
-                    TODO("Not yet implemented")
+                    // TODO("Not yet implemented")
                 }
 
                 override fun onCodeSent(
@@ -57,52 +74,62 @@ class OTPActivity : AppCompatActivity() {
                     forceResendingToken: PhoneAuthProvider.ForceResendingToken
                 ) {
                     super.onCodeSent(verifyId, forceResendingToken)
-                    dialog!!.dismiss()
+                    dialog.dismiss()
                     verificationId = verifyId
                     val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
                     imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
-                    binding!!.otpView.requestFocus()
+                    binding.otpView.requestFocus()
                 }
             }).build()
 
         PhoneAuthProvider.verifyPhoneNumber(options)
-        binding!!.otpView.setOtpCompletionListener { otp ->
-            val credential = PhoneAuthProvider.getCredential(verificationId!!, otp)
-            auth!!.signInWithCredential(credential)
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        val userId = auth!!.currentUser!!.uid
-                        val database = FirebaseDatabase.getInstance().getReference("users")
-                        val userRef = database.child(userId)
+    }
 
-                        userRef.addListenerForSingleValueEvent(object : ValueEventListener {
-                            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                                if (dataSnapshot.exists()) {
-                                    val user = dataSnapshot.getValue(User::class.java)
-                                    if (user != null) {
-                                        if (!user.name.isNullOrEmpty() && !user.profileImage.isNullOrEmpty()) {
-                                            startActivity(Intent(this@OTPActivity, MainActivity::class.java))
-                                            finishAffinity()
-                                        } else {
-                                            startActivity(Intent(this@OTPActivity, SetupProfileActivity::class.java))
-                                            finishAffinity()
-                                        }
-                                    }
-                                } else {
-                                    startActivity(Intent(this@OTPActivity, SetupProfileActivity::class.java))
-                                    finishAffinity()
-                                }
-                            }
-
-                            override fun onCancelled(databaseError: DatabaseError) {
-                                // Handle possible errors.
-                                Toast.makeText(this@OTPActivity, "Failed to read value.", Toast.LENGTH_SHORT).show()
-                            }
-                        })
-                    } else {
-                        Toast.makeText(this@OTPActivity, "Failed", Toast.LENGTH_SHORT).show()
-                    }
+    private fun handleOtpCompletion() {
+        binding.otpView.setOtpCompletionListener { otp ->
+            val credential = PhoneAuthProvider.getCredential(verificationId, otp)
+            auth.signInWithCredential(credential).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    checkUserProfile()
+                } else {
+                    Toast.makeText(this@OTPActivity, "Failed", Toast.LENGTH_SHORT).show()
                 }
+            }
+        }
+    }
+
+    private fun checkUserProfile() {
+        val userId = auth.currentUser?.uid ?: return
+        val database = FirebaseDatabase.getInstance().getReference("users")
+        val userRef = database.child(userId)
+
+        userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    val user = dataSnapshot.getValue(User::class.java)
+                    if (user != null) {
+                        if (!user.name.isNullOrEmpty() && !user.profileImage.isNullOrEmpty()) {
+                            startNewActivity(MainActivity::class.java)
+                        } else {
+                            startNewActivity(SetupProfileActivity::class.java)
+                        }
+                    }
+                } else {
+                    startNewActivity(SetupProfileActivity::class.java)
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle possible errors.
+                Toast.makeText(this@OTPActivity, "Failed to read value.", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun startNewActivity(clazz: Class<*>) {
+        Intent(this@OTPActivity, clazz).apply {
+            startActivity(this)
+            finishAffinity()
         }
     }
 }
