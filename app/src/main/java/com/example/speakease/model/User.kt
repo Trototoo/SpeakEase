36dec3332806fa7512b12bdad78aaac4078608d0
Oleksandr Nazarenko <sa44ko@gmail.com>
2023-06-05 19:46:38 +0300

    Changed User class to data class.

--- app/src/main/java/com/example/speakease/model/User.kt
+++ app/src/main/java/com/example/speakease/model/User.kt
@@ -1,20 +1,8 @@
 package com.example.speakease.model
 
-class User {
-    var uid:String? = null
-    var name:String? = null
-    var phoneNumber:String? = null
-    var profileImage:String? = null
-    constructor(){}
-    constructor(
-        uid:String?,
-        name:String?,
-        phoneNumber:String?,
-        profileImage:String?
-    ) {
-        this.uid = uid
-        this.name = name
-        this.phoneNumber = phoneNumber
-        this.profileImage = profileImage
-    }
-}
+data class User(
+    val uid:String? = null,
+    val name:String? = null,
+    val phoneNumber:String? = null,
+    val profileImage:String? = null
+)
