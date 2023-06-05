31bec6c6860c32e322c67f25f2c622733b091020
Oleksandr Nazarenko <sa44ko@gmail.com>
2023-06-05 20:11:12 +0300

    Implemented saveUserToFirebase function

--- app/src/main/java/com/example/speakease/SetupProfileActivity.kt
+++ app/src/main/java/com/example/speakease/SetupProfileActivity.kt
@@ -1,5 +1,6 @@
 package com.example.speakease
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -14,10 +15,25 @@ class SetupProfileActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
     private lateinit var storage: FirebaseStorage
+    private lateinit var dialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setup_profile)
+    }
+
+    private fun saveUserToFirebase(name: String, imageUrl: String) {
+        val uid = auth.uid
+        val phone = auth.currentUser?.phoneNumber
+        val user = User(uid!!, name, phone, imageUrl)
+        database.reference
+            .child("users")
+            .child(uid)
+            .setValue(user)
+            .addOnCompleteListener {
+                dialog.dismiss()
+                navigateToMainActivity()
+            }
     }
 
     private fun initFirebase() {
