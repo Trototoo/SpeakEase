caa61005e29f777bdde5e5362f055e29a51b17b5
Oleksandr Nazarenko <sa44ko@gmail.com>
2023-06-07 22:45:53 +0300

    Added check for valid number and fixed bug with invalid number.

--- app/src/main/java/com/example/speakease/VerificationActivity.kt
+++ app/src/main/java/com/example/speakease/VerificationActivity.kt
@@ -3,8 +3,12 @@ package com.example.speakease
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.speakease.databinding.ActivityVerificationBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.i18n.phonenumbers.NumberParseException
+import com.google.i18n.phonenumbers.PhoneNumberUtil
+import com.google.i18n.phonenumbers.Phonenumber
 
 class VerificationActivity : AppCompatActivity() {
 
@@ -38,7 +42,11 @@ class VerificationActivity : AppCompatActivity() {
     private fun setContinueButtonOnClick() {
         binding.continueBtn.setOnClickListener {
             val phoneNumber = binding.editNumber.text.toString()
+            if (isValidPhoneNumber(phoneNumber)) {
             navigateToOTPActivity(phoneNumber)
+            } else {
+                Toast.makeText(this, "Invalid phone number", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
@@ -54,5 +62,20 @@ class VerificationActivity : AppCompatActivity() {
             it.putExtra("phoneNumber", phoneNumber)
             startActivity(it)
         }
+    }
+
+    private fun isValidPhoneNumber(phoneNumber: String): Boolean {
+        val phoneUtil = PhoneNumberUtil.getInstance()
+
+        try {
+            // You should specify the region code.
+            // If you're not sure about the region, you can just use "ZZ".
+            val numberProto: Phonenumber.PhoneNumber = phoneUtil.parse(phoneNumber, "ZZ")
+            return phoneUtil.isValidNumber(numberProto)
+        } catch (e: NumberParseException) {
+            println("NumberParseException was thrown: $e")
+        }
+
+        return false
     }
 }
