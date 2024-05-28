f6694ca5ee013299196a9c914e05fdd88ddceba0
Oleksandr <sa44ko@gmail.com>
2024-05-28 17:55:44 +0300

    Refactored MainActivity to use FirebaseRepository.

--- app/src/main/java/com/example/speakease/MainActivity.kt
+++ app/src/main/java/com/example/speakease/MainActivity.kt
@@ -1,29 +1,20 @@
 package com.example.speakease
 
 import android.app.ProgressDialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
-import com.example.speakease.Constants.USERS_PATH
-import com.example.speakease.Constants.USER_PRESENCE
 import com.example.speakease.adapter.UserAdapter
 import com.example.speakease.databinding.ActivityMainBinding
-import com.example.speakease.model.User
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.example.speakease.repositories.FirebaseRepository
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var database: FirebaseDatabase
-    private lateinit var users: ArrayList<User>
     private lateinit var usersAdapter: UserAdapter
     private lateinit var dialog: ProgressDialog
-    private lateinit var user: User
-    private lateinit var currentUserId: String
+    private val repository = FirebaseRepository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,44 +33,22 @@ class MainActivity : AppCompatActivity() {
             setCancelable(false)
         }
 
-        database = FirebaseDatabase.getInstance()
-        users = ArrayList()
-
-        currentUserId = FirebaseAuth.getInstance().uid!!
-
-        database.reference.child(USERS_PATH)
-            .child(currentUserId)
-            .addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    user = snapshot.getValue(User::class.java)!!
-                }
-
-                override fun onCancelled(error: DatabaseError) {}
-            })
+        usersAdapter = UserAdapter(arrayListOf())
+        binding.mRec.layoutManager = GridLayoutManager(this, 2)
+        binding.mRec.adapter = usersAdapter
     }
 
     private fun setupUsersList() {
-        usersAdapter = UserAdapter(users)
-        binding.mRec.layoutManager = GridLayoutManager(this, 2)
-        binding.mRec.adapter = usersAdapter
-
-        database.reference.child(USERS_PATH).addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                users.clear()
-                for (userSnapshot in snapshot.children) {
-                    val user: User = userSnapshot.getValue(User::class.java)!!
-                    if (user.uid != FirebaseAuth.getInstance().uid) users.add(user)
-                }
-                usersAdapter.notifyDataSetChanged()
-            }
-
-            override fun onCancelled(error: DatabaseError) {}
-        })
+        repository.getUsers(
+            onSuccess = { users ->
+                usersAdapter.updateUsers(users)
+            },
+            onError = { error -> Log.e("Error", error.message) }
+        )
     }
 
     private fun setUserOnlineStatus() {
-        database.reference.child(USER_PRESENCE).child(currentUserId)
-            .setValue("Online")
+        repository.setUserPresenceStatus("Online")
     }
 
     override fun onResume() {
@@ -89,8 +58,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
-        database.reference.child(USER_PRESENCE)
-            .child(currentUserId)
-            .setValue("Offline")
+        repository.setUserPresenceStatus("Offline")
     }
 }
